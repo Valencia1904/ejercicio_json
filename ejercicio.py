3af815497efc0600e5a4e783cd033b9e8a500a8a
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-02-24 17:47:02 +0000

    Resolucion de ejercicio2

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 fich=open("becas_premios_0.json")
 becas=json.load(fich)
@@ -20,4 +21,22 @@ for beca in becas:
 	print "Descripcion"
 	print beca["descripcion"]
 	print ""
-	print "----------------------------------------------------------------"
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+raw_input("Pulsa intro para iniciar ejercicio 2")
+os.system('clear')
+
+for beca in becas:
+	print "Titulo: ",beca["titulo"]
+	
+	if type(beca["documentacion"]["documentacion_item"]) == list:
+		for documentacion in beca["documentacion"]["documentacion_item"]:
+			print "Documento: ",documentacion["titulo"]
+			print "...................................................."
+	else:
+		print "Documento",beca["documentacion"]["documentacion_item"]["titulo"]
+	
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+	
+
